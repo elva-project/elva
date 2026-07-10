21a6ab075229db4ee3aa4156d4deb1759aef0a77
Jakob Zahn <jakob.zahn@mailbox.org>
2026-07-10 10:00:42 +0200

    Add tests for the `auth` module

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,12 +1,18 @@
-import pytest
+from base64 import b64decode
 
-from elva.auth import Auth, DummyAuth, Secret
+from cryptography.fernet import Fernet
+from pytest import mark, raises
+
+from elva.auth import Auth, DummyAuth, Secret, basic_authorization_header, fernet
 
 # use AnyIO pytest plugin
-pytestmark = pytest.mark.anyio
+pytestmark = mark.anyio
+
+# alias
+parametrize = mark.parametrize
 
 
-def test_wrapper():
+def test_wrapper() -> None:
     """
     The `Secret` container redacts a wrapper on string conversion
     and in its string representiation.
@@ -46,8 +52,119 @@ def test_wrapper():
     str(new_wrapper) == new_redact
 
 
-def test_auth_class():
-    """Unspecified credential checking logic results in an error."""
+def test_fernet() -> None:
+    """
+    The fernet manager class is obtained from a given `Secret`.
+    """
+    # setup
+    secret = Secret("foo")
+
+    f = fernet(secret)
+
+    # we get a `Fernet` class
+    assert isinstance(f, Fernet)
+
+    # for completeness, the symmetric encryption and decryption works
+    message = b"secret message"
+    token = f.encrypt(message)
+
+    assert f.decrypt(token) == message
+
+
+@parametrize(
+    "message",
+    (
+        b"",
+        b"secret",
+        "🙈".encode(),
+    )
+)
+def test_remote_fernet(message: bytes) -> None:
+    """
+    Two `Fernet` classes can be used for encryption and decryption with
+    the same secret.
+
+    Implicitely, the key derivation function yields the same key with
+    the same secret.
+
+    Arguments:
+        message: the message to encrypt and decrypt in both directions.
+    """
+    # setup
+    secret = Secret("foo")
+
+    a = fernet(secret)
+    b = fernet(secret)
+
+    # encrypt with `a`
+    token = a.encrypt(message)
+
+    # decrypt with `b`
+    assert b.decrypt(token) == message
+
+    # encrypt with `b`
+    token = b.encrypt(message)
+
+    # decrypt with `a`
+    assert a.decrypt(token) == message
+
+
+@parametrize(
+    ("username", "password"),
+    (
+        ("nobody", ""),
+        ("Jane", "foo"),
+        ("John", "bar"),
+    ),
+)
+def test_basic_authorization_header(username: str, password: str) -> None:
+    """
+    The header mapping is composed as expected by a server.
+
+    Arguments:
+        username: the user name.
+        password: the password.
+    """
+    header = basic_authorization_header(username, password)
+
+    # we get only this header
+    assert len(header) == 1
+    assert "Authorization" in header
+
+    # the scheme and credentials are separated by a single whitespace;
+    # only scheme and credentials are present
+    value = header["Authorization"]
+    scheme, credentials = value.split(" ")
+
+    # scheme and credentials are as expected
+    assert scheme == "Basic"
+    assert b64decode(credentials.encode()).decode().split(":") == [username, password]
+
+
+@parametrize(
+    ("username", "password"),
+    (
+        ("a:b", ""),
+        ("c:", "foo"),
+        (":d", "bar"),
+    ),
+)
+def test_basic_authorization_header_fail(username: str, password: str) -> None:
+    """
+    Header creation fails when there is a colon `:` in the username.
+
+    Arguments:
+        username: the user name.
+        password: the password.
+    """
+    with raises(ValueError):
+        basic_authorization_header(username, password)
+
+
+def test_auth_class() -> None:
+    """
+    Unspecified credential checking logic results in an error.
+    """
     auth = Auth()
 
     assert hasattr(auth, "log")
@@ -55,36 +172,58 @@ def test_auth_class():
     username = "some-user"
     password = "secret"
 
-    with pytest.raises(NotImplementedError):
+    with raises(NotImplementedError):
         auth.check(username, password)
 
 
-async def test_async_auth_class():
-    """Defining `check` as coroutine should work as expected"""
-    PASSWORD = "1234"
+@parametrize(
+    ("username", "correct", "wrong"),
+    (
+        ("nobody", "", "something"),
+        ("Jane", "foo", "nofoo"),
+        ("John", "bar", ""),
+    ),
+)
+async def test_async_auth_class(username: str, correct: str, wrong: str) -> None:
+    """
+    Defining `check` as coroutine works as expected.
+
+    Arguments:
+        username: the user name.
+        correct: the correct password.
+        wrong: the wrong password.
+    """
 
     class TestAuth(Auth):
         async def check(self, username, password):
-            return password == PASSWORD
+            return password == correct
 
     auth = TestAuth()
 
-    assert await auth.check("anybody", PASSWORD)
-    assert not await auth.check("nobody", "abcd")
+    assert await auth.check(username, correct)
+    assert not await auth.check(username, wrong)
 
 
-def test_dummy_auth_class():
-    """Dummy authentication works as expected."""
+@parametrize(
+    ("username", "password", "expected"),
+    (
+        ("nobody", "", False),
+        ("Jane", "foo", False),
+        ("John", "bar", False),
+        ("", "", True),
+        ("Jane", "Jane", True),
+        ("John", "John", True),
+    ),
+)
+def test_dummy_auth_class(username: str, password: str, expected: bool) -> None:
+    """
+    Dummy authentication works as expected.
+
+    Arguments:
+        username: the user name.
+        password: the password.
+        expected: expected return value of the authentication check.
+    """
     auth = DummyAuth()
 
-    username = "Jane"
-    password = "nobody_knows"
-
-    assert not auth.check(username, password)
-
-    username = "Jon"
-    password = "Jon"
-    assert auth.check(username, password)
-
-
-# TODO: add tests for LDAPAuth, which requires a reliable test server
+    assert auth.check(username, password) is expected
